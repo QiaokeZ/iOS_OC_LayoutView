1ca6e24cc25a60ab9aca86200d972b73d5ef271e
admin <admin@admindeMacBook-Pro.local>
2019-03-17 21:36:00 +0800

    ...

---
+++ LayoutView/LayoutView/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  LayoutView
+//
+//  Created by admin on 2019/3/17.
+//  Copyright © 2019 com.etraffic.EasyCharging. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
